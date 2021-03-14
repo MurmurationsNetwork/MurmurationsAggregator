f700e8a2bcccf591960d2be1737a0b1fe2f5b456
Bryan Mutai <mutaiwork@gmail.com>
2021-03-14 15:55:32 +0300

    fix schema selector and pagination key styles

--- components/Layout.js
+++ components/Layout.js
@@ -23,7 +23,7 @@ function Layout({ children, setSchema }) {
                     <Box display={["block", "block", "none"]}>
                         <TabLinks />
                     </Box>
-                    <Select pos="absolute" top={["24%", "21%", "15%"]} right="10%" zIndex="1000" width={["70%", "70%", "30%"]} borderColor="brand.100" borderWidth="2px" bg="brand.600" placeholder="Select schema" onChange={(e) => handleChange(e.target.value)}>
+                    <Select pos="absolute" top={["20%", "21%", "15%"]} right="10%" zIndex="1000" width={["70%", "70%", "30%"]} borderColor="brand.100" borderWidth="2px" bg="brand.500" placeholder="Select schema" onChange={(e) => handleChange(e.target.value)}>
                         <option value="complementary_currencies-v1"> Complementary Currency schema </option>
                         <option value="murmurations_map-v1">Murmurations map schema</option>
                         <option value="all">Apply all schemas</option>
